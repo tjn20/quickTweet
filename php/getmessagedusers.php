7f9d21c2f05684d74cfcc4ff5bc013c8351a7b2b
TJN <142109365+tjn20@users.noreply.github.com>
2023-11-14 00:32:36 +0400

    Update getmessagedusers.php

--- php/getmessagedusers.php
+++ php/getmessagedusers.php
@@ -11,33 +11,7 @@ include_once "database.php";
 
 $user_id=$_SESSION['unique_id'];
 
-/* $result="";
-$sql=mysqli_query($conn,"SELECT * FROM users,messages where (unique_id=msg_sender_id OR unique_id=msg_receive_id) AND unique_id<>{$user_id}");
 
-while($row=mysqli_fetch_assoc($sql)){
-    $sql2=mysqli_query($conn,"SELECT * FROM messages WHERE (msg_receive_id={$row['unique_id']}
-    OR msg_sender_id={$row['unique_id']}) AND (msg_sender_id={$user_id}
-    OR msg_receive_id={$user_id}) ORDER BY message_id DESC LIMIT 1");
-
-$get=mysqli_fetch_assoc($sql2);
-if(!mysqli_num_rows($sql2)>0){
-    $result="No message";
- }
-else{
- (strlen($get['message'])>25)?$msg=substr($get['message'],0,20).'...':$msg=$get['message'];
- (($user_id==$get['msg_sender_id'])?$you="You: ":$you=""); 
-}
-
-if($result!="No message"){
-    $output.='<a href="message.php?username='.$row['username'].'">
-    <img src="php/images/'.$row['img'].'" alt=""><div class="user-message">
-    <h5>@'.$row['username'].'</h5><p>'.$you.$msg.'</p></div></a>';
-}
-
-}
-
-
- echo $output; */
 
  $result = "";
  $output = "";
